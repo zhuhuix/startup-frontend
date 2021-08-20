962f5b8382988927073e34459b7fc997ec1eb661
zhuhuix <oncwnuG0rUsG-3z-ro14Fgtq_qpk@git.weixin.qq.com>
2021-08-20 11:12:11 +0800

    增加省市区级联选择

--- src/views/user/center.vue
+++ src/views/user/center.vue
@@ -39,18 +39,22 @@
             <ul class="user-info">
               <li>
                 <div style="height: 100%">
-                  <svg-icon icon-class="login" /> 登录账号
+                  <i class="el-icon-user" /> 登录账号
                   <div class="user-right">{{ user.userName }}</div>
                 </div>
               </li>
               <li>
-                <svg-icon icon-class="user1" /> 用户昵称
+                <i class="el-icon-picture-outline-round" /> 用户昵称
                 <div class="user-right">{{ user.nickName }}</div>
               </li>
               <li v-if="user.dept!=null"><svg-icon icon-class="dept" /> 公司名称 <div class="user-right"> {{ user.dept.name }}</div></li>
               <li>
-                <svg-icon icon-class="phone" /> 电话号码
+                <i class="el-icon-mobile-phone" /> 电话号码
                 <div class="user-right">{{ user.phone }}</div>
+              </li>
+              <li>
+                <i class="el-icon-location-information" /> 所在地区
+                <div class="user-right">{{ user.province }} {{ user.city }} {{ user.country }}</div>
               </li>
             </ul>
           </div>
@@ -83,9 +87,20 @@
                 </el-form-item>
                 <el-form-item label="性别">
                   <el-radio-group v-model="form.gender" style="width: 178px">
-                    <el-radio label="1">男</el-radio>
-                    <el-radio label="2">女</el-radio>
+                    <el-radio :label="1">男</el-radio>
+                    <el-radio :label="2">女</el-radio>
                   </el-radio-group>
+                </el-form-item>
+                <el-form-item label="城市" prop="city" size="mini">
+                  <el-cascader
+                    v-model="selectedOptions"
+                    size="mini"
+                    :options="options"
+                    filterable
+                    clearable
+                    style="width: 250px"
+                    @change="handleChange"
+                  />
                 </el-form-item>
                 <el-form-item label="">
                   <el-button
@@ -111,6 +126,7 @@ import { getToken } from '@/utils/auth'
 import store from '@/store'
 import { saveUser } from '@/api/user'
 import Avatar from '@/assets/images/avatar.png'
+import { regionData, CodeToText, TextToCode } from 'element-china-area-data'
 export default {
   name: 'Center',
   components: { myUpload },
@@ -124,6 +140,8 @@ export default {
         'Authorization': getToken()
       },
       form: {},
+      options: regionData,
+      selectedOptions: [],
       rules: {
         nickName: [
           { required: true, message: '请输入用户昵称', trigger: 'blur' },
@@ -140,10 +158,26 @@ export default {
   },
   created() {
     store.dispatch('getInfo').then(() => {
-      this.form = { id: this.user.id, nickName: this.user.nickName, gender: this.user.gender, phone: this.user.phone }
+      this.form = { id: this.user.id, nickName: this.user.nickName, gender: this.user.gender, phone: this.user.phone,
+        province: this.user.province, city: this.user.city, country: this.user.country }
+      if (this.form.country !== '') {
+        this.selectedOptions = [TextToCode[this.form.province].code, TextToCode[this.form.province][this.form.city].code, TextToCode[this.form.province][this.form.city][this.form.country].code]
+      } else {
+        this.selectedOptions = [TextToCode[this.form.province].code, TextToCode[this.form.province][this.form.city].code]
+      }
     })
   },
   methods: {
+    handleChange(value) {
+      this.form.province = ''
+      this.form.city = ''
+      this.form.country = ''
+      for (let i = 0; i < this.selectedOptions.length; i++) {
+        if (i === 0) { this.form.province = CodeToText[this.selectedOptions[i]] }
+        if (i === 1) { this.form.city = CodeToText[this.selectedOptions[i]] }
+        if (i === 2) { this.form.country = CodeToText[this.selectedOptions[i]] }
+      }
+    },
     toggleShow() {
       this.showDialog = !this.showDialog
     },
