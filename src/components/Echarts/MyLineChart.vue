f036e8f707f852ca16909bd7deff79cb43997766
zhuhuix <oncwnuG0rUsG-3z-ro14Fgtq_qpk@git.weixin.qq.com>
2022-04-18 17:14:21 +0800

    增加疫情折线图

---
+++ src/components/Echarts/MyLineChart.vue
@@ -0,0 +1,80 @@
+<template>
+  <div ref="myChart" :style="{height:height,width:width,margin:margin}" />
+</template>
+
+<script>
+import echarts from 'echarts'
+import debounce from 'lodash/debounce'
+import { addListener, removeListener } from 'resize-detector'
+
+const Count = 0
+
+export default {
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    },
+    margin: {
+      type: String,
+      default: '0px'
+    },
+    autoResize: {
+      type: Boolean,
+      default: true
+    },
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      chart: null,
+      resizeCount: Count
+    }
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler(val) {
+        this.chart.setOption(val)
+      }
+    }
+  },
+  created() {
+    // 防抖处理
+    this.resize = debounce(this.resize, 300)
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    removeListener(this.$refs.myChart, this.resize)
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$refs.myChart)
+      addListener(this.$refs.myChart, this.resize)
+      this.chart.setOption(this.option)
+    },
+
+    resize() {
+      console.log('resize', ++this.resizeCount)
+      this.chart.resize()
+    }
+  }
+}
+</script>
+
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,67 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <!-- <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div> -->
+
+    <my-line-chart :option="lineChartOption" />
+
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import MyLineChart from '@/components/Echarts/MyLineChart'
 
 export default {
   name: 'Dashboard',
+  components: { MyLineChart },
+  data() {
+    return {
+      lineChartOption: {
+        xAxis: {
+          name: '日期',
+          data: ['4/1', '4/2', '4/3', '4/4', '4/5', '4/6', '4/7', '4/8', '4/9', '4/10', '4/11', '4/12', '4/13', '4/14', '4/15', '4/16'],
+          boundaryGap: false,
+          axisTick: {
+            show: false
+          }
+        },
+
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          },
+          padding: [5, 10]
+        },
+        yAxis: {
+          name: '人数',
+          axisTick: {
+            show: false
+          }
+        },
+        legend: {
+          data: ['新增本土']
+        },
+        series: [{
+          name: '新增本土', itemStyle: {
+            normal: {
+              color: '#FF005A',
+              lineStyle: {
+                color: '#FF005A',
+                width: 2
+              }
+            }
+          },
+          smooth: true,
+          type: 'line',
+          data: [12, 5, 10, 7, 3, 3, 13, 5, 7, 14, 4, 3, 2, 4, 2, 7],
+          animationDuration: 2800,
+          animationEasing: 'cubicInOut'
+        }]
+      }
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
